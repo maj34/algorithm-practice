a3b6a15bf27e1945e7cd891c53a9600af994ab13
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-08-26 22:59:05 +0900

    Title: 배수의 개수, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250826/배수의 개수/number-of-multipliers.py
+++ 250826/배수의 개수/number-of-multipliers.py
@@ -1,6 +1,26 @@
 a = int(input())
-a = int(input())
-a = int(input())
-a = int(input())
-a = int(input())
-a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+i = int(input())
+j = int(input())
+
+num_list = [a, b, c, d, e, f, g, h, i, j]
+three = 0
+five = 0
+
+for i in num_list:
+    if i % 3 == 0:
+        three += 1
+        if i % 15 == 0:
+            five += 1
+    elif i % 5 == 0:
+        five += 1
+    else:
+        continue
+
+print(three, five)
