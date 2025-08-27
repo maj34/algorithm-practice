2b278f2e001a959a84d784b77a655ab5e466bc72
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-08-27 23:37:58 +0900

    Title: 특정 원소들의 합, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250827/특정 원소들의 합/sum-of-specific-elements.py
@@ -0,0 +1,12 @@
+arr = [
+    list(map(int, input().split()))
+    for _ in range(4)
+]
+
+ssum = 0
+for i in range(4):
+    for j in range(4):
+        if j <= i:
+            ssum += arr[i][j]
+
+print(ssum)
