09c939e02b100e3f3434bb877f3e6a7ddec23002
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-08-28 13:37:55 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250828/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+def square(n, m):
+    for i in range(n):
+        for j in range(m):
+            print(1, end="")
+        print()
+
+square(n, m)
