c79d358562ef5fb1271ae26235d9a313596585e6
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-08-31 14:40:52 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250831/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 250831/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,11 +1,21 @@
 n1, n2 = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+a = list(map(str, input().split()))
+b = list(map(str, input().split()))
+
+# def continuous_subarray(a, b):
+#     for i in range(n1 - n2 + 1):
+#         if a[i:i+n2] == b:
+#             return "Yes"
+#     return "No"
+
+# print(continuous_subarray(a, b))
 
 def continuous_subarray(a, b):
-    for i in range(n1 - n2 + 1):
-        if a[i:i+n2] == b:
-            return "Yes"
+    a_tmp = ' '.join(a)
+    b_tmp = ' '.join(b)
+
+    if b_tmp in a_tmp:
+        return "Yes"
     return "No"
 
 print(continuous_subarray(a, b))
