39aa537fdf466ae5df756a020ed592fd615ca834
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-08-31 13:40:04 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250831/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
+++ 250831/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -5,8 +5,6 @@ import math
 def is_prime(n):
     if n == 1 :
         return False
-    if n == 2:
-        return True 
     for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
             return False
