bbc3c1b0d26674e98f4e19af84faad3318f1a8ef
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-09-03 23:51:24 +0900

    Title: DateTime to DateTime, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250903/DateTime to DateTime/datetime-to-datetime.py
+++ 250903/DateTime to DateTime/datetime-to-datetime.py
@@ -1,3 +1,10 @@
 a, b, c = map(int, input().split())
 
-print((a*60*24 + b*60 + c) - (11*60*24 + 11*60 + 11))
+if a < 11:
+    print(-1)
+elif a == 11 and b < 11:
+    print(-1)
+elif a == 11 and b == 11 and c < 11:
+    print(-1)
+else:
+    print((a*60*24 + b*60 + c) - (11*60*24 + 11*60 + 11))
