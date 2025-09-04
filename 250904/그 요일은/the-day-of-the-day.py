db7f3df3b396fe549c34caba112c5b1d885e043a
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-09-04 23:06:35 +0900

    Title: 그 요일은, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/그 요일은/the-day-of-the-day.py
+++ 250904/그 요일은/the-day-of-the-day.py
@@ -4,5 +4,11 @@ A = input()
 day = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", 'Sun']
 
-day_num = (sum(day[:m2-1]) + d2) - (sum(day[:m1-1]) + d1)
-print(day_num//7+1)
+day_num = (sum(day[:m2-1]) + d2) - (sum(day[:m1-1]) + d1) + 1
+
+cnt = day_num // 7
+for i in range(day_num%7):
+    if i % 7 == day_week.index(A):
+        cnt += 1
+
+print(cnt)
