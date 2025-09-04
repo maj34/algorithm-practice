db55209bfa91c697f7d950d3079337148cf50d1b
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-09-04 23:46:14 +0900

    Title: 여러가지 진수변환, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/여러가지 진수변환/various-numeral-system-transformations.py
+++ 250904/여러가지 진수변환/various-numeral-system-transformations.py
@@ -2,11 +2,8 @@ N, B = map(int, input().split())
 
 def notation(N, B):
     digits = []
-    while True:
-        if N <= 1:
-            digits.append(N)
-            break
-        digits.append(N%B)
+    while N > 0:
+        digits.append(N % B)
         N //= B
     for i in digits[::-1]:
         print(i, end="")
