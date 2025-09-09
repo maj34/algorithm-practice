744765cbbc4904644c0249c7842fd8648778ff92
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-09-09 19:11:41 +0900

    Title: 좀 더 어려운 수학 점수, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250909/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,11 @@
+Am, Ae = map(int, input().split())
+Bm, Be = map(int, input().split())
+
+if Am > Bm:
+    print("A")
+elif Bm > Am:
+    print("B")
+elif Am == Bm and Ae > Be:
+    print("A")
+else:
+    print("B")
