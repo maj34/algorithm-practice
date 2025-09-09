76521300a818f7b2a418d324372edf3eec9ee0af
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-09-09 17:55:28 +0900

    Title: 4번의 크기 비교, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250909/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,7 @@
+A = int(input())
+B, C, D, E = map(int, input().split())
+
+print(1) if A > B else print(0)
+print(1) if A > C else print(0)
+print(1) if A > D else print(0)
+print(1) if A > E else print(0)
