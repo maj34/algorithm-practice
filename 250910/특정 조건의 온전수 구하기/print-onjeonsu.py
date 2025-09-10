23a97e96dfeed81ac19157a81c91ced11d2a1246
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-09-10 10:01:42 +0900

    Title: 특정 조건의 온전수 구하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250910/특정 조건의 온전수 구하기/print-onjeonsu.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for i in range(1, N+1):
+    if i%2==0:
+        continue
+    elif i%10==5:
+        continue
+    elif i%3==0 and i%9!=0:
+        continue
+    else:
+        print(i, end=" ")
