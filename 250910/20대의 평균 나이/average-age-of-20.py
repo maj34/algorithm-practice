f10132705b319a85d0ed9886787d4a5739e7360e
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-09-10 15:50:37 +0900

    Title: 20대의 평균 나이, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250910/20대의 평균 나이/average-age-of-20.py
+++ 250910/20대의 평균 나이/average-age-of-20.py
@@ -2,12 +2,8 @@ age_sum = 0
 num = 0
 
 while True:
-    try:
-        age = int(input())
-    except EOFError:
-        break
-
-    if age < 30:
+    age = int(input())
+    if 20 <= age < 30:
         age_sum += age
         num += 1
     else:
