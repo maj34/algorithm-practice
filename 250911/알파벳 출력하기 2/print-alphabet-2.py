4d9e5b05fe6ee07e69387a755b0d7282f22af24a
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-09-11 20:53:26 +0900

    Title: 알파벳 출력하기 2, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250911/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+num=0
+for i in range(N):
+    print("  "*i, end="")
+    for j in range(N):
+        if i <= j:
+            print(chr(ord("A")+num), end=" ")
+            if (ord("A")+num)==90:
+                num-=26
+            num+=1
+    print()
