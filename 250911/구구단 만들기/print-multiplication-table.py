2cb0d38215e4faa177bcecfa9e7145f7529028af
마민정 (MinJeong Ma) <75362328+maj34@users.noreply.github.com>
2025-09-11 16:22:50 +0900

    Title: 구구단 만들기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250911/구구단 만들기/print-multiplication-table.py
@@ -0,0 +1,8 @@
+A, B = map(int, input().split())
+
+for i in range(1, 10):
+    for j in range(B, A-1, -2):
+        if j > A:
+           print(f"{j} * {i} = {i*j} / ", end="")
+        else:
+            print(f"{j} * {i} = {i*j}")
