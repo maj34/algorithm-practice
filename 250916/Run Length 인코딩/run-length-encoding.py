ba48113ab3d2c012f8135d1619d6675971a7d327
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-09-16 18:31:49 +0900

    Title: Run Length 인코딩, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250916/Run Length 인코딩/run-length-encoding.py
+++ 250916/Run Length 인코딩/run-length-encoding.py
@@ -2,14 +2,14 @@ A = input()
 
 char=""
 cnt=1
-for i in range(len(A)-1):
-    if A[i] == A[i+1]:
+for i in range(1, len(A)):
+    if A[i] == A[i-1]:
         cnt+=1
     else:
-        char+=A[i]
+        char+=A[i-1]
         char+=str(cnt)
         cnt=1
-char+=A[i+1]
+char+=A[i]
 char+=str(cnt)
 
 print(len(char))
