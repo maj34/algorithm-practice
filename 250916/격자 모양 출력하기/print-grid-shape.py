d9963d00c5654e82f1d46029f2e341cd97eb9f7e
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-09-16 16:48:05 +0900

    Title: 격자 모양 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250916/격자 모양 출력하기/print-grid-shape.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+
+arr = [
+    [0] * N
+    for _ in range(N)
+]
+
+for _ in range(M):
+    r, c = map(int, input().split())
+    arr[r-1][c-1] = r*c
+
+for i in arr:
+    for j in i:
+        print(j, end=" ")
+    print()
