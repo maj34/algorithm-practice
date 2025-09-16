9a0878f9f9299262e6de1722a194ebb801350bbd
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-09-16 11:45:17 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250916/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 250916/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,20 +1,28 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-minimum=min(arr)
-maximum=max(arr)
-max_arr=[]
-min_arr=[]
+max_profit = 0
+for i in range(len(arr)-1):
+    for j in range(i+1, len(arr)):
+        if arr[j] - arr[i] >= max_profit:
+            max_profit = arr[j] - arr[i]
 
-# 최솟값을 기준으로 최댓값 찾기
-for i in arr[arr.index(minimum):]:
-    max_arr.append(i)
-right_max = max(max_arr)
+print(max_profit)
 
-# 최댓값을 기준으로 최솟값 찾기
-for j in arr[:arr.index(maximum)+1]:
-    min_arr.append(j)
-left_min = min(min_arr)
+# minimum=min(arr)
+# maximum=max(arr)
+# max_arr=[]
+# min_arr=[]
 
-profit = max(right_max - minimum, left_min - maximum)
-print(profit)
+# # 최솟값을 기준으로 최댓값 찾기
+# for i in arr[arr.index(minimum):]:
+#     max_arr.append(i)
+# right_max = max(max_arr)
+
+# # 최댓값을 기준으로 최솟값 찾기
+# for j in arr[:arr.index(maximum)+1]:
+#     min_arr.append(j)
+# left_min = min(min_arr)
+
+# profit = max(right_max - minimum, left_min - maximum)
+# print(profit)
