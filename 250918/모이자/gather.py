e60acf15680a55e9b6432c6beb845efe5681c6a9
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-09-18 12:26:49 +0900

    Title: 모이자, Time: 51ms, Memory: 17MB, Status: Passed - Codetree

--- 250918/모이자/gather.py
+++ 250918/모이자/gather.py
@@ -2,16 +2,13 @@ n = int(input())
 A = list(map(int, input().split()))
 
 import sys
-# min_val = sys.maxsize
-min_val = []
+min_val = sys.maxsize
 
 for i in range(n):
     distance = 0
     for j in range(n):
         distance += A[j] * abs(j-i)
-        # if distance <= min_val:
-        #     min_val = distance
-    min_val.append(distance)
-    # print(distance)
+    if distance <= min_val:
+        min_val = distance
 
-print(min(min_val))
+print(min_val)
