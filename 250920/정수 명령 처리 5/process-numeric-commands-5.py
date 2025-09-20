0e89a0587001273e2ae641fa50e1bedf3af3c59e
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-09-20 15:38:33 +0900

    Title: 정수 명령 처리 5, Time: 132ms, Memory: 21MB, Status: Passed - Codetree

---
+++ 250920/정수 명령 처리 5/process-numeric-commands-5.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+arr = []
+
+for _ in range(N):
+    command = input().split()
+    
+    if command[0] == "push_back":
+        arr.append(int(command[1]))
+    elif command[0] == "pop_back":
+        arr.pop()
+    elif command[0] == "size":
+        print(len(arr))
+    elif command[0] == "get":
+        print(arr[int(command[1])-1])
