be165bcdb5e6fcc509369b06ca770fa216db4a9f
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-03 22:31:19 +0900

    Title: 오목, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251003/오목/O-mok.py
+++ 251003/오목/O-mok.py
@@ -14,23 +14,12 @@ for i in range(len(board)):
                 if 0 <= i + m[0] < len(board) or 0 <= j + m[1] < len(board[0]):
                     if board[i + m[0]][j + m[1]] == winner: # 두 번째 돌도 맞음
                         winner_arr.append([i + m[0], j + m[1]])
-                        # print("winner_arr", winner_arr)
+                        # print("winner_arr:", winner_arr)
                         for k in range(2, 5):
                             if 0 <= i + m[0] < len(board) or 0 <= j + m[1] < len(board[0]):
                                 if board[i + (m[0] * k)][j + (m[1] * k)] == winner:
                                     winner_arr.append([i + (m[0] * k), j + (m[1] * k)])
-                                else:
-                                    break
-                            else:
-                                break
                         if len(winner_arr) == 5:
-                            # print("winner_arr:", winner_arr)
                             print(winner)
                             print(winner_arr[2][0] + 1, winner_arr[2][1] + 1)
-                            # exit(0)
-                    else:
-                        break
-                else:
-                    break
-        
 
