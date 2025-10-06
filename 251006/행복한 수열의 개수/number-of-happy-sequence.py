53c8b632ffc95ce369fc52c80491e308bb1f4f84
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-06 21:48:52 +0900

    Title: 행복한 수열의 개수, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 251006/행복한 수열의 개수/number-of-happy-sequence.py
+++ 251006/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,7 +1,6 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# 행 기준 연속적인 숫자 판단
 def consecutive(array):
     is_consecutive = 0
     if len(array) == 1:
@@ -16,6 +15,7 @@ def consecutive(array):
             if prev_num == arr[i] == arr[i+1]:
                 cnt += 1
             else:
+                cnt = 1
                 prev_num = arr[i+1]
 
             if cnt >= m:
@@ -23,7 +23,6 @@ def consecutive(array):
                 break
     return is_consecutive
             
-# 열 기준 연속적인 숫자 판단
 col_val = [[] for _ in range(n)]
 for i in range(n):
     for j in range(n):
