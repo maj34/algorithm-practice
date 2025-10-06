1d061985c0f1504dbe755afa26992af1bc5bf3d7
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-06 20:10:24 +0900

    Title: 최고의 13위치 2, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 251006/최고의 13위치 2/best-place-of-13-2.py
+++ 251006/최고의 13위치 2/best-place-of-13-2.py
@@ -6,11 +6,7 @@ for i in range(n):
     for j in range(n-2):
         for k in range(n):
             for l in range(n-2):
-                if i == k and j == l:
-                    continue
-                elif i == k and j+1 == l:
-                    continue
-                elif i == k and j+2 == l:
+                if i == k and abs(j-l)<=2:
                     continue
                 else:
                     arr_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + \
