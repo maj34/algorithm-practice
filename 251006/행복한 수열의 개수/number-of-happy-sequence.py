635c5252a72ca4443018dd6c81168775787dc0da
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-06 21:48:04 +0900

    Title: 행복한 수열의 개수, Time: 59ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251006/행복한 수열의 개수/number-of-happy-sequence.py
+++ 251006/행복한 수열의 개수/number-of-happy-sequence.py
@@ -5,14 +5,19 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 def consecutive(array):
     is_consecutive = 0
     if len(array) == 1:
-        if 1 >= m:
+        cnt = 1
+        if cnt >= m:
             return 1
     
     for arr in array:
+        prev_num = arr[0]
         cnt = 1
         for i in range(n-1):
-            if arr[i] == arr[i+1]:
+            if prev_num == arr[i] == arr[i+1]:
                 cnt += 1
+            else:
+                prev_num = arr[i+1]
+
             if cnt >= m:
                 is_consecutive += 1
                 break
@@ -24,4 +29,4 @@ for i in range(n):
     for j in range(n):
         col_val[i].append(grid[j][i])
 
-print(consecutive(grid) + consecutive(col_val))
+print(consecutive(grid)+consecutive(col_val))
