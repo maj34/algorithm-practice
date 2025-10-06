a70ff38782bea4792dbde52d5acdcc34b590d680
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-06 21:37:17 +0900

    Title: 행복한 수열의 개수, Time: 58ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 251006/행복한 수열의 개수/number-of-happy-sequence.py
+++ 251006/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,35 +1,27 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-def consecutive_cnt(array):
-    max_res = 0
-    answer = 0
-    start = None
-    for arr in array:
-        if arr == start: # consecutive
-            start = arr
-            answer += 1
-        else: # not consecutive
-            start = arr
-            if answer > max_res:
-                max_res = answer
-            answer = 1
-    if answer > max_res:
-        max_res = answer
-    return max_res
+# 행 기준 연속적인 숫자 판단
+def consecutive(array):
+    is_consecutive = 0
+    if len(array) == 1:
+        if 1 >= m:
+            return 1
     
-ans = 0
-for g in grid:
-    if consecutive_cnt(g) >= m:
-        ans += 1
+    for arr in array:
+        cnt = 1
+        for i in range(n-1):
+            if arr[i] == arr[i+1]:
+                cnt += 1
+            if cnt >= m:
+                is_consecutive += 1
+                break
+    return is_consecutive
+            
+# 열 기준 연속적인 숫자 판단
+col_val = [[] for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        col_val[i].append(grid[j][i])
 
-rev_arr = [[] for _ in range(len(grid))]
-for i in range(len(grid[0])):
-    for j in range(len(grid)):
-        rev_arr[i].append(grid[j][i])
-
-for g in rev_arr:
-    if consecutive_cnt(g) >= m:
-        ans += 1
-
-print(ans)
+print(consecutive(grid) + consecutive(col_val))
