4e4200545e74e04f2badaf5a81386fa6fae53f2d
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-06 20:08:24 +0900

    Title: 최고의 13위치 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251006/최고의 13위치 2/best-place-of-13-2.py
+++ 251006/최고의 13위치 2/best-place-of-13-2.py
@@ -1,11 +1,20 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-max_sum = []
+max_sum = 0
 for i in range(n):
     for j in range(n-2):
-        arr_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-        max_sum.append(arr_sum)
+        for k in range(n):
+            for l in range(n-2):
+                if i == k and j == l:
+                    continue
+                elif i == k and j+1 == l:
+                    continue
+                elif i == k and j+2 == l:
+                    continue
+                else:
+                    arr_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + \
+                            arr[k][l] + arr[k][l+1] + arr[k][l+2]
+                    max_sum = max(max_sum, arr_sum)
 
-max_sum.sort(reverse=True)
-print(max_sum[0] + max_sum[1])
+print(max_sum)
