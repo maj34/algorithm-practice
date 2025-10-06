a07505df96b2d942b68e1f48b6da32bfb48f581e
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-06 20:00:04 +0900

    Title: 최고의 13위치 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251006/최고의 13위치 2/best-place-of-13-2.py
+++ 251006/최고의 13위치 2/best-place-of-13-2.py
@@ -1,10 +1,11 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-max_sum = 0
-for i in range(n-1):
+max_sum = []
+for i in range(n):
     for j in range(n-2):
-        arr_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j] + arr[i+1][j+1] + arr[i+1][j+2]
-        max_sum = max(max_sum, arr_sum)
+        arr_sum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+        max_sum.append(arr_sum)
 
-print(max_sum)
+max_sum.sort(reverse=True)
+print(max_sum[0] + max_sum[1])
