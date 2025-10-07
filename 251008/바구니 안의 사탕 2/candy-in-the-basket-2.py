9a99b820ed86e782d187da7c557c70497efa8f86
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-08 00:19:18 +0900

    Title: 바구니 안의 사탕 2, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251008/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,19 @@
+N, K = map(int, input().split())
+
+candy_dict = {}
+for _ in range(N):
+    c, p = map(int, input().split())
+    candy_dict[p] = c
+
+candy_dict = dict(sorted(candy_dict.items()))
+candy_keys = candy_dict.keys()
+
+max_candy = 0
+for i in range(list(candy_keys)[0], list(candy_keys)[-1]-K+1):
+    candy = 0
+    for j in range(i-K, i+K+1):
+        if j in candy_keys:
+            candy += candy_dict[j]
+    max_candy = max(max_candy, candy)
+    
+print(max_candy)
