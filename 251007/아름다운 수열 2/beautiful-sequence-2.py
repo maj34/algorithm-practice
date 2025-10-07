f6563a56417f7de4a60464906cad5f89fa534efb
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-07 23:14:42 +0900

    Title: 아름다운 수열 2, Time: 1097ms, Memory: 18MB, Status: Time Limit Exceed - Codetree

--- 251007/아름다운 수열 2/beautiful-sequence-2.py
+++ 251007/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,15 +1,15 @@
+from itertools import permutations
+
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-from itertools import permutations
-perm = []
-for i in permutations(B, M):
-    perm.append(list(i))
-
 num = 0
-for j in range(N-M+1):
-    if A[j:j+M] in perm:
-        num += 1
+for i in range(N-M+1):
+    sub = A[i:i+M]
+    for j in permutations(B, M):
+        if list(j) == sub:
+            num += 1
+            break
 
 print(num)
