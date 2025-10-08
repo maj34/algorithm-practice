a45ef4c43fd8f54840d2ba6d6203c79f0e4c42f5
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-08 10:14:59 +0900

    Title: 밭의 높이를 고르게하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251008/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -0,0 +1,13 @@
+import sys
+
+N, H, T = map(int, input().split())
+arr = list(map(int, input().split()))
+
+min_cost = sys.maxsize
+for i in range(N-T+1):
+    cost = 0
+    for j in range(i, i+T):
+        cost += abs(arr[j]-H)
+    min_cost = min(min_cost, cost)        
+
+print(min_cost)
