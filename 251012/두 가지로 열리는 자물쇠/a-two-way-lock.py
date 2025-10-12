293a22a90e4d9b7ebfca51ed06c53ace626b661d
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-12 20:55:18 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251012/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,27 @@
+N = int(input())
+a1, b1, c1 = map(int, input().split())
+a2, b2, c2 = map(int, input().split())
+
+llst = [i for i in range(1, N+1)]
+
+def valid_dial(num):
+    lst = []
+    for i in range(-2, 3):
+        lst.append(llst[llst.index(num)+i])
+    return lst
+
+a1_lst, b1_lst, c1_lst = valid_dial(a1), valid_dial(b1), valid_dial(c1)
+a2_lst, b2_lst, c2_lst = valid_dial(a2), valid_dial(b2), valid_dial(c2)
+
+valid_set = set()
+for i in a1_lst:
+    for j in b1_lst:
+        for k in c1_lst:
+            valid_set.add((i, j, k))
+
+for i in a2_lst:
+    for j in b2_lst:
+        for k in c2_lst:
+            valid_set.add((i, j, k))
+
+print(len(valid_set))
