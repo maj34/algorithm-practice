31936e0cf95ff8ce1b8bc0bfe06ea916d76a135d
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-16 18:24:51 +0900

    Title: 갈 수 있는 곳들, Time: 517ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 251016/갈 수 있는 곳들/places-can-go.py
+++ 251016/갈 수 있는 곳들/places-can-go.py
@@ -4,18 +4,18 @@ n, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 points = [tuple(map(int, input().split())) for _ in range(k)]
 
-def bfs(si, sj):
-    q = deque()
-    visited = [[False]*n for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+q = deque()
 
+def bfs(si, sj):
     q.append((si, sj))
     visited[si][sj] = True
-
     while q:
         ci, cj = q.popleft()
         
         for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             ni, nj = ci+di, cj+dj
+            # 갈 수 있는 곳인지 판단
             if 0<=ni<n and 0<=nj<n and not visited[ni][nj] and grid[ni][nj] == 0:
                 q.append((ni, nj))
                 visited[ni][nj] = True
