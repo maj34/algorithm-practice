1fcf33de53daf0cd93d12301de76f3568bfe624d
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-30 00:23:16 +0900

    Title: 정수 사각형 최소 합, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

--- 251030/정수 사각형 최소 합/minimum-sum-path-in-square.py
+++ 251030/정수 사각형 최소 합/minimum-sum-path-in-square.py
@@ -12,13 +12,10 @@ for i in range(N-2, -1, -1):
 # 마지막 열
 for j in range(1, N):
     dp[j][N-1] = dp[j-1][N-1] + grid[j][N-1]
-print(dp)
 
-# # 그 외의 값 채우기
-# for k in range(N-2, -1, -1):
-#     for l in range(N-2, -1, -1):
-#         dp[k][l] = min(dp[k-1][l], dp[k][l+1]) + grid[k][l]
+# 그 외의 값 채우기
+for k in range(1, N):
+    for l in range(N-2, -1, -1):
+        dp[k][l] = min(dp[k-1][l], dp[k][l+1]) + grid[k][l]
 
-# # 출력
-# print(dp)
-# print(dp[N-1][0])
+print(dp[N-1][0])
