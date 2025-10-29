49eeef52816ec4c78d38a664a071adbc0d0bd84e
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-29 14:17:29 +0900

    Title: 사각형 채우기 3, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251029/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+# Bottom-Up
+dp = [0] * (N+1)
+
+for i in range(1, N+1):
+    if i == 1:
+        dp[1] = 2
+    elif i == 2:
+        dp[2] = 7
+    elif i == 3:
+        dp[3] = 22
+    else:
+        dp[i-2]*7 + dp[i-1]*2
+
+print(dp[N]%1000000007)
