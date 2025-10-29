64bd9359b035cf211862bab1ac7bacca3b7db2c7
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-29 13:58:42 +0900

    Title: 계단 오르기, Time: 1097ms, Memory: 24MB, Status: Time Limit Exceed - Codetree

--- 251029/계단 오르기/climbing-stairs.py
+++ 251029/계단 오르기/climbing-stairs.py
@@ -1,8 +1,23 @@
 N = int(input())
 
-dp = [0, 0, 1, 1] + [0] * (N-3)
+# dp = [0, 0, 1, 1] + [0] * (N-3)
 
-for i in range(4, N+1):
-    dp[i] = dp[i-3] + dp[i-2]
+# for i in range(4, N+1):
+#     dp[i] = dp[i-3] + dp[i-2]
 
-print(0 if dp[N]==0 else dp[N]%10007)
+# print(0 if dp[N]==0 else dp[N]%10007)
+
+def stair(N):
+    memo = [-1] * (N+1)
+    if memo[N] != -1: # 기존에 있는 값 반환
+        return memo[N]
+
+    if N <= 1:
+        memo[N] = 0
+    elif 2 <= N <= 3:
+        memo[N] = 1
+    else:
+        memo[N] = stair(N-3) + stair(N-2)
+    return memo[N]
+
+print(0 if stair(N)==0 else stair(N)%10007)
