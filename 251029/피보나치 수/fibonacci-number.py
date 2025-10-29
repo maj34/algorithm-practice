e7da61858fa89796aa286b50447587007c2a6e0d
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-29 13:35:03 +0900

    Title: 피보나치 수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251029/피보나치 수/fibonacci-number.py
+++ 251029/피보나치 수/fibonacci-number.py
@@ -1,10 +1,13 @@
 N = int(input())
 
 dp = [0] * (N+1)
-dp[1] = 1
-dp[2] = 1
 
-for i in range(3, N+1):
-    dp[i] = dp[i-2] + dp[i-1]
+if N <= 1:
+    dp[1] = 1
+else:
+    dp[1] = 1
+    dp[2] = 1
+    for i in range(3, N+1):
+        dp[i] = dp[i-2] + dp[i-1]
 
 print(dp[N])
