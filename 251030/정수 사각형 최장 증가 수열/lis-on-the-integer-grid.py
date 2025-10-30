8f6dc3ff0fa7e687099aa7f622913e69583ab04f
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-30 14:26:01 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 183ms, Memory: 27MB, Status: Runtime Error - Codetree

--- 251030/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 251030/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -3,27 +3,41 @@ from collections import deque
 N = int(input())
 grid = [list(map(int, input().split())) for _ in range(N)]
 
-def bfs(si, sj):
-    q = deque()
-    visited = [[False]* N for _ in range(N)]
+dp = [[-1]*N for _ in range(N)]
 
-    q.append((si, sj, 1))
-    visited[si][sj] = True
-
-    while q:
-        ci, cj, cnt = q.popleft()
-        for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            ni, nj = ci+di, cj+dj
-            if 0<=ni<N and 0<=nj<N and not visited[ni][nj] and grid[ni][nj] > grid[ci][cj]:
-                q.append((ni, nj, cnt+1))
-                visited[ni][nj] = True
+def dfs(i, j):
+    if dp[i][j] != -1:
+        return dp[i][j]
     
-    return cnt
+    dp[i][j] = 1 # 자기자신 포함
+    for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        ni, nj = i+di, j+dj
+        if 0<=ni<N and 0<=nj<N and grid[ni][nj] > grid[i][j]:
+            dp[i][j] = max(dp[i][j], dfs(ni, nj)+1)
+
+    return dp[i][j]
+
+
+# def bfs(si, sj):
+#     q = deque()
+#     visited = [[False]* N for _ in range(N)]
+
+#     q.append((si, sj, 1))
+#     visited[si][sj] = True
+
+#     while q:
+#         ci, cj, cnt = q.popleft()
+#         for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+#             ni, nj = ci+di, cj+dj
+#             if 0<=ni<N and 0<=nj<N and not visited[ni][nj] and grid[ni][nj] > grid[ci][cj]:
+#                 q.append((ni, nj, cnt+1))
+#                 visited[ni][nj] = True
+    
+#     return cnt
 
 max_cnt = 0
 for i in range(N):
     for j in range(N):
-        cnt = bfs(i, j)
-        max_cnt = max(max_cnt, cnt)
+        max_cnt = max(max_cnt, dfs(i, j))
 
 print(max_cnt)
