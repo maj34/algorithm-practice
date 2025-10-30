5e0cc3e07131a3049bcfa4d594736b46846fef6d
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-10-30 15:26:56 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 121ms, Memory: 21MB, Status: Wrong Answer - Codetree

--- 251030/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 251030/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -1,34 +1,53 @@
 from collections import deque
-import sys
 
 N = int(input())
 grid = [list(map(int, input().split())) for _ in range(N)]
 
-min_diff = sys.maxsize
+# 격자 내 최솟값과 최댓값
+min_val = min(map(min, grid))
+max_val = max(map(max, grid))
 
-q = deque()
-q.append((0, 0, [grid[0][0]]))  # 현재까지 경로에 포함된 값들의 list까지 저장
-
-while q:
-    ci, cj, current_path_values = q.popleft()
-
-    # 목적지에 도달한 경우
-    if ci == N - 1 and cj == N - 1:
-        current_max = max(current_path_values)
-        current_min = min(current_path_values)
-        diff = current_max - current_min
+# BFS로 도달 가능한지 확인
+def can_reach(diff, start_min):
+    start_max = start_min + diff
+    if grid[0][0] < start_min or grid[0][0] > start_max:
+        return False
+    
+    q = deque([(0, 0)])
+    visited = [[False] * N for _ in range(N)]
+    visited[0][0] = True
+    
+    while q:
+        i, j = q.popleft()
+        if i == N - 1 and j == N - 1:
+            return True
         
-        min_diff = min(min_diff, diff)
-        continue 
-        
-    for di, dj in [(1, 0), (0, 1)]:
-        ni, nj = ci + di, cj + dj
-        
-        if 0 <= ni < N and 0 <= nj < N:
-            # current_path_values 리스트를 복사하고 새 값을 추가
-            new_path_values = list(current_path_values)
-            new_path_values.append(grid[ni][nj])
-            
-            q.append((ni, nj, new_path_values))
+        for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < N and 0 <= nj < N and not visited[ni][nj]:
+                if start_min <= grid[ni][nj] <= start_max:
+                    visited[ni][nj] = True
+                    q.append((ni, nj))
+    return False
 
-print(min_diff)
+# 이분 탐색으로 최소 차이 구하기
+left, right = 0, max_val - min_val
+answer = right
+
+while left <= right:
+    mid = (left + right) // 2
+    possible = False
+    
+    # 가능한 시작 최솟값 범위 탐색
+    for start_min in range(min_val, max_val - mid + 1):
+        if can_reach(mid, start_min):
+            possible = True
+            break
+    
+    if possible:
+        answer = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(answer)
