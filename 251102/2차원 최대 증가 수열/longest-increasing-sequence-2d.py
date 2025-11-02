891e7e808d97a954b258332cc445391f6cfc88ec
마민정 (MinJeong Ma) <minjeong_ma@korea.ac.kr>
2025-11-02 11:15:00 +0900

    Title: 2차원 최대 증가 수열, Time: 119ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 251102/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[-1] * M for _ in range(N)]
+
+def dfs(i, j):
+    if dp[i][j] != -1:
+        return dp[i][j]
+
+    dp[i][j] = 1  # 자기 자신 포함
+
+    # 오른쪽 + 아래쪽으로 점프 가능한 모든 칸 탐색
+    for ni in range(i + 1, N):
+        for nj in range(j + 1, M):
+            if arr[ni][nj] > arr[i][j]:
+                dp[i][j] = max(dp[i][j], dfs(ni, nj) + 1)
+
+    return dp[i][j]
+    
+ans = 0
+for i in range(N):
+    for j in range(M):
+        ans = max(ans, dfs(i, j))
+
+print(ans)
